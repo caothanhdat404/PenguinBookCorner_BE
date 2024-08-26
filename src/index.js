e38343744fe500c65e7e525113fa7e0ebe03753d
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-26 22:19:29 +0700

    Connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require('dotenv')
+const { default: mongoose } = require("mongoose")
 dotenv.config()
 
 const app = express()
@@ -8,6 +9,15 @@ const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+mongoose.connect(`mongodb+srv://Wiinter:${process.env.MONGO_DB}@cluster.2kzn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`)
+.then(() => {
+    console.log('Connect Db success')
+})
+.catch((err) => {
+    console.log(err)
+})
+
 app.listen(port, () => {
     console.log('Server is running in port: ' + port)
 })
