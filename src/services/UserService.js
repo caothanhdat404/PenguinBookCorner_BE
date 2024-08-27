c808d64555f4fc2b3f84526020aec6e12ded8b96
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-27 15:48:05 +0700

    Hash password when sign-up, & API login, update user, delete user, get all user, get users

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,6 @@
 const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const { generalAccessToken, generalRefreshToken } = require("./JwtService")
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
@@ -13,10 +15,10 @@ const createUser = (newUser) => {
                     message: 'The user is already'
                 })
             }
+            const hash = bcrypt.hashSync(password, 10)
             const createUser = await User.create({
                 email,
-                password,
-                confirmPassword
+                password: hash,
             })
             if (createUser) {
                 resolve({
@@ -31,6 +33,147 @@ const createUser = (newUser) => {
     })
 }
 
+const loginUser = (user) => {
+    return new Promise(async (resolve, reject) => {
+        const { email, password } = user
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The password or user is incorrect'
+                })
+            }
+            const comparePassword = bcrypt.compare(password, checkUser.password)
+            if(!comparePassword) {
+                resolve({
+                    status: 'OK',
+                    message: 'The password or user is incorrect'
+                })
+            }
+            const access_token = await generalAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            const refresh_token = await generalRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                access_token,
+                refresh_token
+            })
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+const updateUser = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+            
+            const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
+            
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedUser
+            })
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+            
+            await User.findByIdAndDelete(id)
+            
+            resolve({
+                status: 'OK',
+                message: 'Delete user success'
+            })
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allUser = await User.find()
+            
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: allUser
+            })
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+const getDetailsUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({
+                _id: id
+            })
+
+            if (user === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+                        
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: user
+            })
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getDetailsUser
 }
