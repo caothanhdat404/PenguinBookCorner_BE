1b77a020ed42664194ba28344090f95ab02527e3
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-29 17:35:35 +0700

    Fix bug: Login not need confirmPassword

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -33,10 +33,10 @@ const createUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     try {
-        const { email, password, confirmPassword } = req.body
+        const { email, password } = req.body
         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
         const isCheckEmail = reg.test(email)
-        if (!email || !password || !confirmPassword ) {
+        if ( !email || !password ) {
             return res.status(200).json({
                 status: 'ERR',
                 message: 'The input is required'
@@ -45,11 +45,6 @@ const loginUser = async (req, res) => {
             return res.status(200).json({
                 status: 'ERR',
                 message: 'The input is email'
-            })
-        } else if (password !== confirmPassword) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The password is equal confirmPassword'
             })
         }
         const response = await UserService.loginUser(req.body)
