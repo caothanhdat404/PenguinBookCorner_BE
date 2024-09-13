4a4086839e20f9b5597b3ee63fd2e1233c4e57c0
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-13 22:28:40 +0700

    Fix model data of product

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema(
     {
         name: { type: String, required: true, unique: true },
-        image: { type: String, required: true },
+        image: { type: String },
         type: { type: String, required: true },
         price: { type: Number, required: true },
         countInStock: { type: Number, required: true },
         rating: { type: Number, required: true },
         description: { type: String },
         sold: { type: Number },
-        discount: { type: Number }
+        discount: { type: Number },
     },
     {
         timestamps: true
